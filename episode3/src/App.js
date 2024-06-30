d476d71d5973ed04162712ee9ee7abb207ad3a01
IT22074072 <it22074072@my.sliit.lk>
2024-06-30 13:10:20 +0530

    Conditional Rendering

--- episode3/src/App.js
+++ episode3/src/App.js
@@ -2,6 +2,22 @@ import './App.css';
 
 function App() {
 
+  const age = 4;
+  const isGreen = true;
+    return (
+    <div className="App">
+      {age >= 18 ? <h1>OVER AGE</h1> : <h1> UNDER AGE</h1>}
+      <h1 style={{color: isGreen ? "green" : "red"}}>THIS HAS A COLOR</h1> 
+    
+      {isGreen && <button>THIS IS A BUTTON</button>}
+    </div>)
+
+}
+
+
+
+/*function App() {
+
   const age = 40
   if (age >= 18){
     return <h1 className='age'>Over age</h1>;
@@ -9,7 +25,9 @@ function App() {
     return <div className="App"><h1>Under age</h1> </div>;
   }
   
-}
+}*/
+
+
 
 /*
 function App() {
