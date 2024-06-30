1383548675b92d07389e95b0b0503489370b515b
IT22074072 <it22074072@my.sliit.lk>
2024-06-30 12:48:32 +0530

    if-else statements

--- episode3/src/App.js
+++ episode3/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 
 function App() {
+
+  const age = 40
+  if (age >= 18){
+    return <h1 className='age'>Over age</h1>;
+  } else{
+    return <div className="App"><h1>Under age</h1> </div>;
+  }
+  
+}
+
+/*
+function App() {
   return (
     <div className="App">
       <h1 className='name'>Dinithi</h1>
     </div>
   );
-}
+} */
 
 export default App;
